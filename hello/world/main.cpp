93533c2ba9bbcb7c740987680fd550bcb9521f1d
WenVencent <2745379013@qq.com>
2025-10-26 23:08:35 +0800

    将带有注释的CMakeList.txt文件上传

--- hello/world/main.cpp
+++ hello/world/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
-    printf("I will publish this program!");
+    printf("I will publish this program!\n");
+    cout<<"2025.10.26 I learnt a CMake course for one hour"<<endl;
     return 0;
 }
